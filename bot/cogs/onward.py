53b62dc56860d6ed4e4ea2bedb20e817aa1fdf53
User <user@Manablast-Mac.local>
2021-06-04 16:04:30 -0500

    channel switch

--- bot/cogs/onward.py
+++ bot/cogs/onward.py
@@ -9,7 +9,7 @@ class Onward(commands.Cog):
     #Create an Onward PVP/PVE Event for reacts
     @commands.command(name="onward", type=["PVP","PVE"], host="", help="Posts an Onward LFG post by typing '!onward PVP/PVE HOST'")
     async def onward(self, ctx, type, host):
-        ctx.channel = self.client.get_channel(796486608278519838)
+        ctx.channel = self.client.get_channel(796486423456907297)
         message = await ctx.send(f"**{host} is hosting an Onward {type} lobby**\n React for points and join.")
         await ctx.message.delete()
         await message.add_reaction('\N{THUMBS UP SIGN}')
