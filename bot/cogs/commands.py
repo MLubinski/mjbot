a52b44341afc3595234d3be86db1e19073b4fd1a
User <user@Manablast-Mac.local>
2021-06-04 15:53:15 -0500

    fixes for test server

--- bot/cogs/commands.py
+++ bot/cogs/commands.py
@@ -6,14 +6,14 @@ class Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.has_role('Admin')
+    #@commands.has_role('Admin')
     @commands.command(name='channels', hidden=True)
     async def print_channels(self, ctx):
         for guild in self.client.guilds:
             for channel in guild.text_channels:
                 await ctx.send(f"{channel.name} {channel.id}")
 
-    @commands.has_role('Admin')
+    #@commands.has_role('Admin')
     @commands.command(hidden=True)
     async def clear(self, ctx, amount=100) :
         await ctx.channel.purge(limit=amount)
