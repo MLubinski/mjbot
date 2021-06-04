110e749566db63332f1e9a7e3e0cf1c25b245ed1
User <user@Manablast-Mac.local>
2021-06-04 13:27:56 -0500

    moved commands into their own cogs

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from cogwatch import Watcher
 import os
 
 client = commands.Bot(command_prefix="!")
@@ -8,23 +9,18 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 @client.event
 async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Doing bot things"))
-    print("I am online")
-
-@client.command(name='channels')
-async def print_channels(ctx):
-    for guild in client.guilds:
-        for channel in guild.text_channels:
-            await ctx.send(f"{channel.name} {channel.id}")
+    watcher = Watcher(client, path='bot/cogs', debug=False)
+    await watcher.start()
+    print("Bot is ready.")
 
 @client.command()
-async def clear(ctx, amount=100) :
-    await ctx.channel.purge(limit=amount)
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
-#!create [PvP/E event] [Host:] 0:00
-@client.command(name="onward", type=["PVP","PVE"], host="", help="Sets up an Onward LFG post")
-async def new_onward_lfg(ctx, type, host):
-    ctx.channel = client.get_channel(850366126528397322)
-    await ctx.send(f"{host} is starting an Onward {type} lobby. React for points and join.")
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
 
 
 client.run(token)
