3545128d957c7af6eb429673cf2a1f8449a99f63
User <user@Manablast-Mac.local>
2021-06-07 19:39:14 -0500

    more counting enhancements

--- bot/cogs/onward.py
+++ bot/cogs/onward.py
@@ -14,7 +14,7 @@ class Onward(commands.Cog):
     #Create an Onward PVP/PVE lobby for reactions.
     @commands.command(name="onward", type=["PVP","PVE"], host="", help="Posts an Onward LFG post by typing '!onward PVP/PVE HOST'")
     #@commands.cooldown(rate=1, per=5)
-    async def onward(self, ctx, type):
+    async def onward(self, ctx, *type):
         if self.open_lobby:
             await ctx.message.delete()
             await ctx.message.author.send("There is already another lobby open. Go join that one.")
@@ -38,14 +38,22 @@ class Onward(commands.Cog):
     @commands.command(name="end", help="This command will end the open lobby and make room for another one.\n **!end**", pass_context = True)
     async def end(self, ctx):
         users = set()
+        author = ctx.author.name
         #print(f"The start. Last message is {self.last_message}.")
         channel = self.client.get_channel(self.last_message.channel.id)
         message = await channel.fetch_message(self.last_message.id)
         for each in message.reactions:
             async for user in each.users():
-                users.add(user)
-        reaction_counter = await ctx.send(f"The following users played in this lobby: {', '.join(user.name for user in users)}")
+                if user.bot == False:
+                    users.add(user)
 
+        if len(users) > 0:
+            description = f"Thanks for playing **@{', '.join(str(user.name) for user in users)}**!"
+        else:
+            description = f"No one joined this lobby { author }. You better find new friends."
+
+        embed = discord.Embed(title="Recent Lobby Report", description=description)
+        reaction_counter = await ctx.send(embed=embed)
         await self.last_message.delete()
         await ctx.message.delete()
         await asyncio.sleep(5)
