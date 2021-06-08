b95727ff07a17cd7deb1fb23604b994a5c3fe07f
User <user@Manablast-Mac.local>
2021-06-08 08:00:56 -0500

    fixes for mj server

--- bot/cogs/commands.py
+++ bot/cogs/commands.py
@@ -6,16 +6,18 @@ class Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    #@commands.has_role('Admin')
+    @commands.has_role('Admin')
+    @commands.cooldown(rate=1, per=60)
     @commands.command(name='channels', hidden=True)
     async def print_channels(self, ctx):
         for guild in self.client.guilds:
             for channel in guild.text_channels:
                 await ctx.send(f"{channel.name} {channel.id}")
 
-    #@commands.has_role('Admin')
+    @commands.has_role('Admin')
+    @commands.cooldown(rate=1, per=60)
     @commands.command(hidden=True)
-    async def clear(self, ctx, amount=10) :
+    async def clear(self, ctx, amount=1) :
         await ctx.channel.purge(limit=amount)
 
 def setup(client):
--- bot/cogs/onward.py
+++ bot/cogs/onward.py
@@ -1,6 +1,7 @@
 import discord, os, asyncio
 from discord.ext import commands
 from typing import Optional
+import datetime
 
 class Onward(commands.Cog):
 
@@ -8,12 +9,15 @@ class Onward(commands.Cog):
         self.client = client
         self.last_message = ""
         self.open_lobby = False
-        self.channel = 796486608278519838
-        self.emoji = '\N{THUMBS UP SIGN}'
+        self.lfg_channel = 850366126528397322
+        self.report_channel = 850448818548899881
+        self.mj_lfg_hannel = 799871582897438750
+        self.mj_report_channel = 827259886681194556
+        self.emoji = ':smalllogo:'
 
     #Create an Onward PVP/PVE lobby for reactions.
-    @commands.command(name="onward", type=["PVP","PVE"], host="", help="Posts an Onward LFG post by typing '!onward PVP/PVE HOST'")
-    #@commands.cooldown(rate=1, per=5)
+    @commands.command(name="onward", type=["PVP","PVE"], host="", help="Posts an Onward LFG post.\n Usage:**'!onward PVP/PVE'**")
+    @commands.cooldown(rate=1, per=3)
     async def onward(self, ctx, *type):
 
         if self.open_lobby:
@@ -24,24 +28,26 @@ class Onward(commands.Cog):
             host = ctx.author.name
             type = ' '.join(type)
 
-            ctx.channel = self.client.get_channel(self.channel)
+            ctx.channel = self.client.get_channel(self.mj_lfg_channel)
             #message = await ctx.send(f"**{host} is hosting an Onward {type} lobby**\n React for points and join.")
             embed=discord.Embed(title=f"{host} is hosting an Onward {type} lobby.", description=f"React to this message to receive your points for the monthly leaderboard.")
             embed.add_field(name='Host', value=host, inline=True)
             embed.add_field(name='Type', value=type, inline=True)
             embed.set_thumbnail(url='https://i.imgur.com/cAJixfU.png')
             msg = await ctx.send(embed=embed)
-            await ctx.message.delete()
             await msg.add_reaction(self.emoji)
+            await asyncio.sleep(1)
+            await ctx.message.delete()
             self.last_message = msg
             self.open_lobby = True
             #print(f"Last message is {message.id}. The lobby is {self.open_lobby}")
 
     #Ends the Onward PVP/PVE lobby that was previously opened.
-    @commands.command(name="end", help="This command will end the open lobby and make room for another one.\n **!end**", pass_context = True)
+    @commands.command(name="end", help="This command will end the open lobby and make room for another one.\n Usage: **!end**", pass_context = True)
     async def end(self, ctx):
         users = set()
         author = ctx.author.name
+        time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
         #print(f"The start. Last message is {self.last_message}.")
         channel = self.client.get_channel(self.last_message.channel.id)
         message = await channel.fetch_message(self.last_message.id)
@@ -49,28 +55,27 @@ class Onward(commands.Cog):
             async for user in each.users():
                 if user.bot == False:
                     users.add(user)
-                    #points_add = await ctx.send(f"Example point add statement for { user }.")
-                    #await asyncio.sleep(5)
-                    #await points_add.delete()
 
         if len(users) > 0:
-            description = f"Thanks for playing **{', '.join(str(user.name) for user in users)}**!"
+            description = f"Thanks for playing **{', '.join(str(f'<@!{user.id}>') for user in users)}**!"
         else:
             description = f"No one joined this lobby { author }. You better find new friends."
 
         embed = discord.Embed(title="Recent Lobby Report", description=description)
+        embed.set_footer(text=f"Lobby closed at {time}")
+        ctx.channel = self.client.get_channel(self.mj_report_channel)
         reaction_counter = await ctx.send(embed=embed)
         await self.last_message.delete()
         await ctx.message.delete()
-        await asyncio.sleep(5)
+        #await asyncio.sleep(1)
         #await reaction_counter.delete()
 
         self.open_lobby = False
 
-def get_channel(ctx, name=None):
-    for channel in ctx.guild.channels:
-        if channel.name == name:
-            return channel.id
+#def get_channel(ctx, name=None):
+#    for channel in ctx.guild.channels:
+#        if channel.name == name:
+#            return channel.id
 
 def setup(client):
     client.add_cog(Onward(client))
